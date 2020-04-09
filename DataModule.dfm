87ca4461f84aa9727c15b8e974d9a23f468d1cfd
pameladilly <pameladilly@hotmail.com>
2020-04-09 20:03:21 -0300

    Criação do banco de dados sqllite

--- DataModule.dfm
+++ DataModule.dfm
@@ -1,5 +1,13 @@
 object DM: TDM
   OldCreateOrder = False
-  Height = 150
-  Width = 215
+  Height = 287
+  Width = 350
+  object FDConect: TFDConnection
+    Params.Strings = (
+      'Database=D:\repos\gerenciador-scripts\database.db'
+      'DriverID=SQLite')
+    LoginPrompt = False
+    Left = 48
+    Top = 80
+  end
 end
--- DataModule.pas
+++ DataModule.pas
@@ -3,10 +3,15 @@ unit DataModule;
 interface
 
 uses
-  System.SysUtils, System.Classes;
+  System.SysUtils, System.Classes, FireDAC.Stan.Intf, FireDAC.Stan.Option,
+  FireDAC.Stan.Error, FireDAC.UI.Intf, FireDAC.Phys.Intf, FireDAC.Stan.Def,
+  FireDAC.Stan.Pool, FireDAC.Stan.Async, FireDAC.Phys, FireDAC.Phys.SQLite,
+  FireDAC.Phys.SQLiteDef, FireDAC.Stan.ExprFuncs, FireDAC.VCLUI.Wait, Data.DB,
+  FireDAC.Comp.Client;
 
 type
   TDM = class(TDataModule)
+    FDConect: TFDConnection;
   private
     { Private declarations }
   public
